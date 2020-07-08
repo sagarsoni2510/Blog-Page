4551cdd23cfe3a247a7b29e448abe5485c3bd092
Sagar Soni <56184941+sagarsoni2510@users.noreply.github.com>
2020-07-08 18:37:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
+var form = document.getElementById("form");
+var subscribe = document.getElementById("subscribe");
+var sign up = document.getElementById("sign up");
+var older = document.getElementById("older");
+var newer = document.getElementById("newer");
---
+++ validation.js
@@ -0,0 +1,7 @@
+function myvalidation(){
+	let myValue = document.getElementById('button button1').value;
+}
+
+function myvalidation(){
+	let myValue = document.getElementById('button button2').value;
+}
